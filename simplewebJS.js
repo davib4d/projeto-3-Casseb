b1d757b7dfafcb01a15d205dbf94590d14e6610a
davib4d <169076054+davib4d@users.noreply.github.com>
2024-10-09 23:50:18 -0300

    Add files via upload

---
+++ simplewebJS.js
@@ -0,0 +1,15 @@
+function home(params) {
+    window.scrollTo(0,50);
+}
+function video(params) {
+    window.scrollTo(0,150);
+}
+function mapa(params) {
+    window.scrollTo(0,530);
+}
+function dicas(params) {
+    window.scrollTo(0,1000);
+}
+function beneficios(params) {
+    window.scrollTo(0,1000);
+}
